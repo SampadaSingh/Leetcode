1603f28c25b648c8ecc33dc1288eeec1fd928dd0
sams-13 <157884703+sams-13@users.noreply.github.com>
2024-02-04 22:33:00 +0545

    Time: 7 ms (94.76%) | Memory: 8.1 MB (67.61%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -7,7 +7,7 @@ public:
     int n=s.length();
     //odd
     for(int i=0; i<n-1;++i){
-        int l=i,r=i;
+        int l=i, r=i;
         while(l>=0 && r<n && s[l]==s[r]){
             l--;r++;
             }

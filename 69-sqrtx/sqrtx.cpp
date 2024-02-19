e0b0d454c33ba2969ca2c5ae18b38855e492c3b8
Sampada <157884703+sams-13@users.noreply.github.com>
2024-02-19 20:04:40 +0545

    Time: 0 ms (100.00%) | Memory: 7.6 MB (20.77%) - LeetSync

---
+++ 69-sqrtx/sqrtx.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+    int  left=0, right=x;
+        while(left<=right){
+           long int mid=(left+right)/2;
+            if(mid*mid==x){
+                return mid;
+            }else if(mid*mid>x){
+                right=mid-1;
+            }else{
+                left=mid+1;
+            }
+        }
+        return left-1;
+    }
+};

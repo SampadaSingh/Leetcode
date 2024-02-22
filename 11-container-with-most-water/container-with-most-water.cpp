7d3b79ceb3d2529b65c61fcff310bf3e222fca9e
Sampada <157884703+sams-13@users.noreply.github.com>
2024-02-22 19:25:41 +0545

    Time: 74 ms (28.00%) | Memory: 61.5 MB (14.51%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-    int start=0, end=height.size()-1;
-    int area, maxarea=0;
+    int start=0;
+    int end=height.size()-1;
+    int area; //stores current area
+    int maxarea=0; //stores max area
 
     while(start<end){
-        area=min(height[start],height[end])*(end-start);
+        area=min(height[start],height[end])*(end-start); //area=height*width
         if(area>maxarea) 
              maxarea=area;
         if(min(height[start],height[end])==height[start])

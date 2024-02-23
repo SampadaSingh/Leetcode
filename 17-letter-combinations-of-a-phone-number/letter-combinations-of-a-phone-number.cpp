aa0681155968fc30e3fae284ea8175e58ad5d6c3
Sampada <157884703+sams-13@users.noreply.github.com>
2024-02-23 22:18:01 +0545

    Time: 0 ms (100.00%) | Memory: 8.3 MB (26.47%) - LeetSync

---
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,29 @@
+class Solution {
+ private:
+    void solve(string digits, string output,int index,vector<string> & ans,string mapping[]){
+    //base case
+    if(index==digits.length()){
+        ans.push_back(output);
+        return;
+    }
+    int number=digits[index]-'0'; //converting character to integer value
+    string value=mapping[number];
+    for(int i=0;i<value.length();i++){
+        output.push_back(value[i]);
+        solve(digits,output,index+1,ans,mapping);
+        //backtrack to remove last character and try new combination
+        output.pop_back();
+    }
+    }
+public:
+    vector<string> letterCombinations(string digits) {
+    vector<string> ans;
+    if(digits.length()==0)
+        return ans;   
+    string output=""; //current combination
+    int index=0; //current position of digits
+    string mapping[10]={" "," ","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"}; //mapping each letters to corresponding digit
+    solve(digits,output,index,ans,mapping);
+    return ans;
+    }
+};

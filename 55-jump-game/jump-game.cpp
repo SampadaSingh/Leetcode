6f2fa9787bc54a713a95acae38d5757e229723d1
Sampada <157884703+sams-13@users.noreply.github.com>
2024-03-01 21:11:03 +0545

    Time: 52 ms (37.24%) | Memory: 50.9 MB (39.90%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int maxReach = 0;
-        for (int i = 0; i < nums.size(); ++i) {
-            if (i > maxReach) return false; // If current index is unreachable
-            maxReach = max(maxReach, i + nums[i]); // Update the maximum reachable index
-            if (maxReach >= nums.size() - 1) return true; // If the last index is reachable
+        int maxReach=0;
+        for(int i=0;i<nums.size();++i){
+            if(i>maxReach)return false;//if current index is unreachable
+            maxReach=max(maxReach,i+nums[i]);//update  the max reachable index
+            if(maxReach>=nums.size()-1) return true;//if the last index is reachable
         }
         return false;
     }

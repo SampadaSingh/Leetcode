e7aac732eca6e9479a5369df4aed77ca5ec669b4
Sampada <157884703+sams-13@users.noreply.github.com>
2024-03-13 21:19:02 +0545

    Time: 14 ms (55.92%) | Memory: 21.2 MB (6.69%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -3,7 +3,7 @@ public:
     int missingNumber(vector<int>& nums) {
     sort(nums.begin(),nums.end());//sorting in ascending order
     int curr=0;
-    for(int i=0;i<nums.size();i++){
+    for(int i=0;i<nums.size();++i){
         if(curr!=nums[i]){
             return curr;
         }

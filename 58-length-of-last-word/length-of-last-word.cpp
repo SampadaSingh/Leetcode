cc8724316b6fe45bd70ac72b884456a4a928d189
Sampada <157884703+sams-13@users.noreply.github.com>
2024-04-01 18:42:41 +0545

    Time: 0 ms (100.00%) | Memory: 7.7 MB (20.26%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.cpp
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        int count=0;
-        bool lastword=false;
-        for(int i=s.length()-1;i>=0;i--){
-            if(s[i]==' '){
-            if(lastword){
-                break;
-           } }else{
-                lastword=true;
+        int count = 0;
+        bool lastword = false;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s[i] == ' ') {
+                if (lastword) {
+                    break;
+                }
+            } else {
+                lastword = true;
                 count++;
             }
         }

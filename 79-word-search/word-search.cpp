73ed3e15151e2d18886ffe088e4e48b28dc8ec58
Sampada <157884703+sams-13@users.noreply.github.com>
2024-04-03 16:48:57 +0545

    Time: 970 ms (45.62%) | Memory: 11.4 MB (61.03%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,32 +1,42 @@
 class Solution {
 public:
-    bool search(vector<vector<char>>&board,string word,int i,int j,int row, int col,int k){
-        if(k>=word.size())return true;
-        if(i<0 || i>=row || j<0 || j>=col || board[i][j]=='.' || word[k]!=board[i][j])return false;//checking if the cell is out of bound
-        if(word.size()==1 && word[k]==board[i][j])return true;
-        board[i][j]='.';//mark as visited
-        bool temp=false;
-        int x[4]={1,0,-1,0};//x coordinates
-        int y[4]={0,1,0,-1};//y coordinates
-        //checking neighbouring cells
-        for(int index=0;index<4;index++){
-           temp=temp || search(board,word,i+x[index],j+y[index],row,col,k+1);
+    bool search(vector<vector<char>>& board, string word, int i, int j, int row, int col, int k) {
+
+        if (k >= word.size())
+            return true;
+        if (i < 0 || i >= row || j < 0 || j >= col || board[i][j] == '.' || word[k] != board[i][j])
+            return false; // checking if the cell is out of bound
+        if (word.size() == 1 && word[k] == board[i][j])
+            return true;
+
+        board[i][j] = '.'; // mark as visited
+        bool temp = false;
+        int x[4] = {1, 0, -1, 0}; // x coordinates
+        int y[4] = {0, 1, 0, -1}; // y coordinates
+
+        // checking neighbouring cells
+        for (int index = 0; index < 4; index++) {
+            temp = temp || search(board, word, i + x[index], j + y[index], row, col, k + 1);
         }
-        board[i][j]=word[k];//reset to current cell
+        board[i][j] = word[k]; // reset to current cell
         return temp;
     }
+
     bool exist(vector<vector<char>>& board, string word) {
-    int row=board.size();
-    if(row==0)return false;
-    int col=board[0].size();  
-    if(word.size()==0) return false; 
-    for(int i=0;i<row;++i){
-        for(int j=0;j<col;++j){
-            if(word[0]==board[i][j]){
-                if(search(board,word,i,j,row,col,0))return true;
+        int row = board.size();
+        if (row == 0)
+            return false;
+        int col = board[0].size();
+        if (word.size() == 0)
+            return false;
+        for (int i = 0; i < row; ++i) {
+            for (int j = 0; j < col; ++j) {
+                if (word[0] == board[i][j]) {
+                    if (search(board, word, i, j, row, col, 0))
+                        return true;
+                }
             }
         }
-    }
-    return false;
+        return false;
     }
 };

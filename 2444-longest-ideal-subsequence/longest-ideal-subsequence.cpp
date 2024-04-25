a043374172f8feab108b0019a055bda7db630380
Sampada <157884703+sams-13@users.noreply.github.com>
2024-04-25 23:14:55 +0545

    Time: 39 ms (85.29%) | Memory: 11.4 MB (84.45%) - LeetSync

---
+++ 2444-longest-ideal-subsequence/longest-ideal-subsequence.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int longestIdealString(string s, int k) {
+
+        int n = s.length();
+        int result = 0;
+        vector<int> t(26, 0);
+
+        for (int i = 0; i < n; i++) {
+            int curr = s[i] - 'a';
+            int left = max(0, curr - k);
+            int right = min(25, curr + k);
+            int longest = 0;
+
+            for (int j = left; j <= right; j++) {
+                longest = max(longest, t[j]);
+            }
+
+            t[curr] = max(t[curr], longest + 1);
+            result = max(result, t[curr]);
+        }
+        return result;
+    }
+};
